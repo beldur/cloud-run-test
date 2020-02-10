488a69c4305e4bf5e0fa4b36c5940056076f1f90
Alexander Lücking <alexluecking@gmail.com>
2020-02-10 21:36:36 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http')
+
+const PORT = process.env.PORT || 8080
+const server = http.createServer((req, res) => {
+    console.log('New Connection')
+
+    res.end('Hello World!')
+})
+
+server.listen(PORT, () => console.log('Listening'))
